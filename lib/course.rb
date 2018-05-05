800010c807b7e1eadf31543bb77c1a83562bff81
Eric Johnson <ericjohnson0216@icloud.com>
2018-05-05 00:41:43 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,13 +1,20 @@
 class Course
-  attr_accessor :title, :schedule, :description
+  attr_accessor :title, :schedule, :description, :id
   @@all = []
+  @@ID = 1
 
   def initialize
+    self.id = @@ID
     @@all << self
+    @@ID += 1
   end
 
   def self.all
     @@all
+  end
+
+  def self.find_by_id
+    self.detect{}
   end
 
   def self.reset_all
